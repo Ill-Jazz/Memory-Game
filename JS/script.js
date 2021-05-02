7e11df8fceb8d1ed0e4b554f2a9aefe50dc379f9
Ill-Jazz <iliasguedoura@gmail.com>
2021-05-02 20:06:36 +0200

    added images , html and styling, still figuring out positioning

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+/*What do I need:
+- cards
+- images
+- random order
+- turn around cards
+- matching function
+
+MUST HAVE FEATURES
+- clicking cards to turn around ---------- addEventlistener
+- randomly position the cards ------------ Math.random + positioning?
+- Readme
+- Explanation on the page itself
+
+NICE TO HAVE FEATURES
+- Make it playable by keyboard
+- Let a user define custom image urls
+- Make it pleasing to look at
+- Multiplayer (local)
+ */
+
+
+let cardUp
+let cardDown
+
+
